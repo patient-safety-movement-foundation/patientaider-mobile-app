987903ed1a141cc8a57a1e90b27a6508c20b7b89
ewolfe <e@ewolfe.me>
2018-11-18 16:49:41 -0800

    refactor: communication with webview

--- App.js
+++ App.js
@@ -2,6 +2,45 @@ import React, { Component } from "react";
 import { SafeAreaView, Alert, Linking } from "react-native";
 import { WebView } from "react-native-webview";
 
+let jsCode = `(function() {
+  var attachEvent = function(elem, event, callback) {
+    event = event.replace(/^on/g, "");
+    if ("addEventListener" in window) {
+      elem.addEventListener(event, callback, false);
+    } else if ("attachEvent" in window) {
+      elem.attachEvent("on" + event, callback);
+    } else {
+      var registered = elem["on" + event];
+      elem["on" + event] = registered
+        ? function(e) {
+            registered(e);
+            callback(e);
+          }
+        : callback;
+    }
+
+    return elem;
+  };
+  var all_links = document.querySelectorAll("a[href]");
+  if (all_links) {
+    for (var i in all_links) {
+      if (all_links.hasOwnProperty(i)) {
+        attachEvent(all_links[i], "onclick", function(e) {
+          if (!new RegExp("^https?://" + location.host, "gi").test(this.href)) {
+            // handle external URL
+            e.preventDefault();
+            window.postMessage(
+              JSON.stringify({
+                external_url_open: this.href
+              })
+            );
+          }
+        });
+      }
+    }
+  }
+})()`;
+
 export default class App extends Component {
   constructor(props) {
     super(props);
@@ -16,7 +55,7 @@ export default class App extends Component {
       data = JSON.parse(data);
     } catch (e) {}
     // check if this message concerns us
-    if ("object" == typeof data && data.external_url_open) {
+    if (typeof data === "object" && data.external_url_open) {
       // proceed with URL open request
       return Alert.alert(
         "External URL",
@@ -31,65 +70,23 @@ export default class App extends Component {
         { cancelable: false }
       );
     }
-  }
 
-  openExternalLink(req) {
-    const isPatientAider = req.url.search("https://patientaider.org") !== -1;
-    const isBlank = req.url.search("about:blank") !== -1;
-
-    if (isPatientAider || isBlank) {
-      return true;
-    } else {
-      Linking.openURL(req.url);
-      return false;
+    if (
+      typeof data === "object" &&
+      data.componentDidMount &&
+      data.componentDidMount === "Topic"
+    ) {
+      this.webView.injectJavaScript(jsCode);
     }
   }
 
   render() {
-    let jsCode = `
-    function injectedJavaScript() {
-      var attachEvent = function(elem, event, callback) {
-        event = event.replace(/^on/g, "");
-        if ("addEventListener" in window) {
-          elem.addEventListener(event, callback, false);
-        } else if ("attachEvent" in window) {
-          elem.attachEvent("on" + event, callback);
-        } else {
-          var registered = elem["on" + event];
-          elem["on" + event] = registered
-            ? function(e) {
-                registered(e);
-                callback(e);
-              }
-            : callback;
-        }
-
-        return elem;
-      };
-      var all_links = document.querySelectorAll("a[href]");
-      if (all_links) {
-        for (var i in all_links) {
-          if (all_links.hasOwnProperty(i)) {
-            attachEvent(all_links[i], "onclick", function(e) {
-              if (!new RegExp("^https?://" + location.host, "gi").test(this.href)) {
-                // handle external URL
-                e.preventDefault();
-                window.postMessage(
-                  JSON.stringify({
-                    external_url_open: this.href
-                  })
-                );
-              }
-            });
-          }
-        }
-      }
-    }
-    injectedJavaScript();`;
-
     return (
       <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
         <WebView
+          ref={c => {
+            this.webView = c;
+          }}
           javaScriptEnabled
           onMessage={this.handleMessage}
           injectedJavaScript={jsCode}
